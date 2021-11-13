f34ea87e574753b0feae042d3265181d900ad7f5
VictoriaLukinova <93930348+VictoriaLukinova@users.noreply.github.com>
2021-11-13 23:58:47 +0300

    Change variable names and variable type

--- hw8/Birthdays_per_week.py
+++ hw8/Birthdays_per_week.py
@@ -1,39 +1,33 @@
 from datetime import datetime, timedelta
 
 users = [
-    {'name': "Anna", "birthday": datetime(year= 1993, month= 11, day= 12, hour= 0)},
-    {'name': "Nat", "birthday": datetime(year= 1993, month= 11, day= 8, hour= 0)},
-    {'name': "Yegor", "birthday": datetime(year= 1993, month= 11, day= 8, hour= 0)},
-    {'name': "Nik", "birthday": datetime(year= 1993, month= 11, day= 11, hour= 0)}
+    {'name': "Anna", "birthday": datetime(year= 1993, month= 11, day= 14, hour= 0)},
+    {'name': "Nat", "birthday": datetime(year= 1993, month= 11, day= 18, hour= 0)},
+    {'name': "Yegor", "birthday": datetime(year= 1993, month= 11, day= 18, hour= 0)},
+    {'name': "Nik", "birthday": datetime(year= 1993, month= 11, day= 16, hour= 0)}
 ]
 
 def get_birthdays_per_week(users):
     current_date = datetime.now()
     delta = timedelta(days= 1)
-    birthdays_dict_int = {
-        0 : [],
-        1 : [],
-        2 : [],
-        3 : [],
-        4 : []
-    }
+    birthdays_list =[[], [], [], [], [],]
     for i in range(6):
-        day = current_date + delta*i
+        date = current_date + delta*i
         for collegue in users:
-            if collegue['birthday'].month == day.month and collegue['birthday'].day == day.day:
-                weekday = day.weekday()
+            if collegue['birthday'].month == date.month and collegue['birthday'].day == date.day:
+                weekday = date.weekday()
                 try:
-                    birthdays_dict_int[weekday].append(collegue['name'])
-                except KeyError:
-                    birthdays_dict_int[0].append(collegue['name'])
-    birthdays_dict_str = {
-        "Monday" : ', '.join(birthdays_dict_int[0]),
-        'Tuesday' : ', '.join(birthdays_dict_int[1]),
-        'Wednesday' : ', '.join(birthdays_dict_int[2]),
-        'Thursday' : ', '.join(birthdays_dict_int[3]),
-        'Friday' : ', '.join(birthdays_dict_int[4])
+                    birthdays_list[weekday].append(collegue['name'])
+                except IndexError:
+                    birthdays_list[0].append(collegue['name'])
+    collegues_birthdays= {
+        "Monday" : ', '.join(birthdays_list[0]),
+        'Tuesday' : ', '.join(birthdays_list[1]),
+        'Wednesday' : ', '.join(birthdays_list[2]),
+        'Thursday' : ', '.join(birthdays_list[3]),
+        'Friday' : ', '.join(birthdays_list[4]),
     }
-    for key, value in birthdays_dict_str.items():
+    for key, value in collegues_birthdays.items():
         if value:
             print(key, ':', value)
 
