fcfe23ee6695bde750cb48c0ef761d96bae45afa
VictoriaLukinova <93930348+VictoriaLukinova@users.noreply.github.com>
2021-11-15 01:30:50 +0300

    Delete IN_CYCLE and change command_list

--- hw9/CLI.py
+++ hw9/CLI.py
@@ -1,7 +1,9 @@
+import collections
+
 CONTACTS_DICT = {
     'Anna' : '0931111111'
 }
-IN_CYCLE = True
+Command_list = collections.namedtuple('Command_list',['command', 'name', 'phone',])
 
 def input_error(func):
     def inner(command_list):
@@ -16,40 +18,38 @@ def input_error(func):
             return 'Give me a name and phone please'
     return inner
 
-def hello(*arg):
+def hello(*arg) -> str:
     return 'How can I help you?'
 
 @input_error
-def add(command_list):
+def add(command_list: Command_list) -> str:
     global CONTACTS_DICT
-    if command_list[1] not in CONTACTS_DICT:
-        CONTACTS_DICT.update({command_list[1]: command_list[2]})
+    if command_list.name not in CONTACTS_DICT:
+        CONTACTS_DICT.update({command_list.name: command_list.phone})
         return 'New contact has been added'
     else:
-        return f"Contact {command_list[1]} in list!"
+        return f"Contact {command_list.name} in list!"
 
 @input_error
-def change(command_list):
+def change(command_list: Command_list) -> str: 
     global CONTACTS_DICT
-    CONTACTS_DICT[command_list[1]] = command_list[2]
-    return f'Contact {command_list[1]} has been changed'
+    CONTACTS_DICT[command_list.name] = command_list.phone
+    return f'Contact {command_list.name} has been changed'
 
 @input_error
-def phone(command_list):
-    return CONTACTS_DICT[command_list[1]]
+def phone(command_list: Command_list) -> str :
+    return CONTACTS_DICT[command_list.name]
 
-def show_all(*arg):
+def show_all(*arg) -> str:
     global CONTACTS_DICT
     for key, value in CONTACTS_DICT.items():
         print(key, ' : ', value)
     return 'End of contacts'
 
-def exit(*arg):
-    global IN_CYCLE
-    IN_CYCLE = False
+def exit(*arg) -> str:
     return "Good bye!"
 
-def command_error(*arg):
+def command_error(*arg) -> str:
     return "Command is not found. Please, try again"
 
 COMMAND_DICT ={
@@ -63,36 +63,40 @@ COMMAND_DICT ={
     'good bye': exit
 }
 
-def get_handler(command):
+def get_handler(command: str):
     try:
         return COMMAND_DICT[command]
     except KeyError:
         return command_error
 
-def change_command(command): #Обработка команды
-    command_list = command.split()
-    if command_list[0].lower() == 'show' and command_list[1].lower() == 'all':
-        delit = command_list.pop(1)
-        command_list[0] = command_list[0] + ' ' + delit
-    if command_list[0].lower() == 'good' and command_list[1].lower() == 'bye':
-        delit = command_list.pop(1)
-        command_list[0] = command_list[0] + ' ' + delit
-    command_list[0] = command_list[0].lower()
-    # if command_list[0] in ('hello', 'show all', 'exit', 'close', 'good bye'):
-    #     if len(command_list) < 3:
-    #         command_list.append('')
-    #         command_list.append('')
-    # if command_list[0] == 'phone' and len(command_list) == 2:
-    #     command_list.append('')
+def change_command(command:str) -> Command_list: #Обработка команды
+    commands = command.split()
+    if len(commands) == 0:
+        command_list = Command_list('', '', '')
+        return command_list    
+    if commands[0].lower() == 'show' and commands[1].lower() == 'all':
+        delete = commands.pop(1)
+        commands[0] = commands[0] + ' ' + delete
+    if commands[0].lower() == 'good' and commands[1].lower() == 'bye':
+        delete = commands.pop(1)
+        commands[0] = commands[0] + ' ' + delete
+    commands[0] = commands[0].lower()
+    if len(commands) == 1:
+        command_list = Command_list(commands[0], '', '')
+    elif len(commands) == 2:
+        command_list = Command_list(commands[0], commands[1], '')
+    elif len(commands) >= 3:
+        command_list = Command_list(commands[0], commands[1], commands[2])
     return command_list
 
 def main():
-    global IN_CYCLE
-    while IN_CYCLE:
+    while True:
         command = input()
         command_list = change_command(command)
-        func = get_handler(command_list[0])
+        func = get_handler(command_list.command)
         print(func(command_list))
+        if command_list.command in ('exit', 'close', 'good bye'):
+            return
 
 if __name__ == '__main__':
     main()
