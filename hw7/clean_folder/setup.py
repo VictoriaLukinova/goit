66ce5f0669a42180d1116f8ee8360a0936a8bf87
VictoriaLukinova <93930348+VictoriaLukinova@users.noreply.github.com>
2021-12-06 20:59:08 +0300

    Add files via upload

---
+++ hw7/clean_folder/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name= 'clean_folder',
+    version= '1',
+    description= 'Folder parser',
+    url= 'https://github.com/VictoriaLukinova/goit/blob/main/hw7/clean_folder/clean_folder/clean.py',
+    author= 'Victoria Lukinova',
+    author_email= 'ViktoriagLuka@gmail.com',
+    license= '',
+    packages= find_namespace_packages(),
+    entry_points ={'console_scripts': ['clean-folder=clean_folder.clean']},
+)
